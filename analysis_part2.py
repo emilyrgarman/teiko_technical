956ffe12dd30778905a5136ac05efacd2282b1ab
emilyrgarman <157059624+emilyrgarman@users.noreply.github.com>
2026-04-10 17:22:56 -0400

    part 2 setup & query

---
+++ analysis_part2.py
@@ -0,0 +1,28 @@
+import sqlite3
+import argparse
+import csv
+import sys
+import os
+
+DB_FILE = "clinical_trial.db"
+
+QUERY = """
+SELECT
+    sm.sample_id AS sample,
+    SUM(cc.count) OVER (PARTITION BY sm.sample_id) AS total_count,
+    p.name AS population,
+    cc.count,
+    ROUND(
+        cc.count * 100.0 / SUM(cc.count) OVER (PARTITION BY sm.sample_id),
+        2
+    ) AS percentage
+FROM samples AS sm
+JOIN counts AS cc ON sm.sample_id = sm.sample_id
+JOIN populations AS p ON p.population_id = cc.population_id
+{where clause}
+ORDER BY
+    sm.sample_id,
+    p.name
+"""
+
+
